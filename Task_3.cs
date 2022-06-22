a7c52dbd4e8e810e59b326a73423ce2cef91fa55
Anybis91 <107985314+Anybis91@users.noreply.github.com>
2022-06-22 19:38:09 +0300

    Solving_tasks_2

---
+++ Task_3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как тебя зовут ?");
+            String name;
+            name = Console.ReadLine();
+            Console.WriteLine("Сколько тебе лет ?");
+            String age;
+            age = Console.ReadLine();
+            Console.WriteLine("Где ты живёшь ?");
+            String city;
+            city = Console.ReadLine();
+            Console.WriteLine("Кем ты работаешь?");
+            String job;
+            job = Console.ReadLine();
+            Console.WriteLine($"Привет {name}! Я знаю что ты живешь в {city}, хорошее место." +
+                $" Получается что ты работаешь {job} в свои {age} лет. Это интересно.");
+        }
+    }
+}
