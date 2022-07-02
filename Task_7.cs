faf4670c662a25dd45386c77d657f36001ae3ccb
Anybis91 <107985314+Anybis91@users.noreply.github.com>
2022-07-02 17:56:50 +0300

    task_7

---
+++ Task_7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во старушек: ");
+            int amountGrandmam;
+            amountGrandmam = Convert.ToInt32(Console.ReadLine());
+            int receptionTimeClient = 10;
+            int timeWaitHour;
+            timeWaitHour = (amountGrandmam * receptionTimeClient) / 60;
+            int timeWaitMin;
+            timeWaitMin = (amountGrandmam * receptionTimeClient) % 60;
+            Console.WriteLine("Вы должны отстоять в очереди " + timeWaitHour + " часа " + timeWaitMin + " минут.");
+        }
+    }
+}
