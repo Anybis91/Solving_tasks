d21add40ed425d631f7c6ff5b935ced277938573
Anybis91 <107985314+Anybis91@users.noreply.github.com>
2022-07-02 16:24:11 +0300

    Task_5

---
+++ Task_5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Иванов";
+            String lastName = "Иван";
+            Console.WriteLine($"Имя {name}, фамилия {lastName}. Что то напутал! Секунду....");
+            String deltaName;
+            deltaName = lastName;
+            lastName = name;
+            name = deltaName;
+            Console.WriteLine($"Имя  {name} , фамилия  {lastName} . Сейчас верно!");
+        }
+    }
+}
