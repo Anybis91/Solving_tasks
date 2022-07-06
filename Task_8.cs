1182148b0d175275dd29f035bd9a0a76b8c85162
Anybis91 <107985314+Anybis91@users.noreply.github.com>
2022-07-06 19:23:29 +0300

    Task_8

---
+++ Task_8.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Введите количество повторений:");
+			int cycleAmount = Convert.ToInt32(Console.ReadLine()); ;
+			Console.WriteLine("Введите сообщение:");
+			String textUser = Console.ReadLine();
+			Console.WriteLine("Цикл запущен:");
+			while (cycleAmount-- > 0)
+			{
+				Console.WriteLine(textUser);
+			}
+
+		}
+	}
+}
