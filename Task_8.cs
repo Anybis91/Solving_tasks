76a7bffa23741e327b292901a519a02044404861
Anybis91 <107985314+Anybis91@users.noreply.github.com>
2022-07-07 12:29:12 +0300

    Update Task_8.cs

--- Task_8.cs
+++ Task_8.cs
@@ -11,6 +11,7 @@ namespace Task_8
 			Console.WriteLine("Введите сообщение:");
 			String textUser = Console.ReadLine();
 			Console.WriteLine("Цикл запущен:");
+
 				for (int amountCycle= cycleAmount; amountCycle > 0; amountCycle--)
 					{
 						Console.WriteLine(textUser);
