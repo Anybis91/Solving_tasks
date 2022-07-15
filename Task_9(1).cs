42eecca0da56c7217010da541a2e0fa476d48d66
Anybis91 <107985314+Anybis91@users.noreply.github.com>
2022-07-15 15:09:14 +0300

    Task_9(1)

---
+++ Task_9(1).cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_9_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите exit для выхода.");
+            string userInput;
+            int amountCycle;
+            string exitCommand = "exit";
+            bool cycleIsEnd=true;
+
+            while(cycleIsEnd)
+            {
+                userInput=Console.ReadLine();
+
+                if (userInput==exitCommand)
+                {
+                    Console.WriteLine("Цикл завершён!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Цикл будет запущен снова!");
+                }
+            }
+        }
+    }
+}
