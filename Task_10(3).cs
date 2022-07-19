1f1ba10e96a8d51209cb415524efb57bc6f8aa6e
Anybis91 <107985314+Anybis91@users.noreply.github.com>
2022-07-19 12:10:21 +0300

    Task_10(3)

---
+++ Task_10(3).cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int variableChange;
+		int stepCycle=7;
+		int startCycleNumber = 5;
+		int endCycleNumber = 100;
+
+		for (variableChange=startCycleNumber; variableChange < endCycleNumber; variableChange = variableChange + stepCycle)
+		{
+			Console.WriteLine(variableChange);
+		}
+	}
+} 
