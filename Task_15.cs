9fa742648f5ad6d9feff70eb56f18c7d3c946269
Anybis91 <107985314+Anybis91@users.noreply.github.com>
2022-07-29 13:55:41 +0300

    Task_15

---
+++ Task_15.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberAttempts=3;
+            string basePassword="873456";
+            string userInputPassword;
+            string baseinfo = "Секретная информация:\nБулочки были выпечены в подвале в Москве.";
+
+            for (int i=0; i<numberAttempts; i++)
+            {
+                Console.WriteLine("Введите пароль для доступа к данным:");
+                userInputPassword = Console.ReadLine();
+
+                if (userInputPassword == basePassword)
+                {
+                    Console.WriteLine(baseinfo);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный пароль! Осталось " + (numberAttempts-i-1) + " попыток!");
+                }
+            }
+        }
+    }
+}
