9cb4254b4cbc8f552ff7cc36e6549d599e220c2d
Anybis91 <107985314+Anybis91@users.noreply.github.com>
2022-07-29 11:46:16 +0300

    Task_14

---
+++ Task_14.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInputName;
+            string userInputSymbol;
+            int numberSymbols;
+            Console.WriteLine("Введите слово:");
+            userInputName = Console.ReadLine();
+            Console.WriteLine("Введите символ:");
+            userInputSymbol= Console.ReadLine();
+            numberSymbols = (userInputName.Length)+2;
+            Console.WriteLine(numberSymbols);
+            Console.WriteLine("Результат:");
+
+            for (int j = 0; j < 1; j++)
+            {
+                Console.WriteLine(userInputSymbol);
+                Console.WriteLine(userInputSymbol+ userInputName + userInputSymbol);
+                Console.WriteLine(userInputSymbol);
+            }
+        }
+    }
+}
