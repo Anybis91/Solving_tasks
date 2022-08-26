0487520a9df3562fd8a5c297e74fc10e7c0994a0
Anybis91 <107985314+Anybis91@users.noreply.github.com>
2022-08-27 00:27:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            char leftBracket = '(';
+            char rightBracket = ')';
+            Console.WriteLine("Введите строку из символов '(' и ')'.");
+            userInput = Console.ReadLine();
+            int valueLeftBracket = +1;
+            int valueRightBracket = -1;
+            int numberBracketsFullExpression = 0;
+            int maxDeepBracket=0;
+
+            foreach (var symbol in userInput)
+            {
+                if (symbol == leftBracket)
+                {
+                    numberBracketsFullExpression += valueLeftBracket;
+
+                    if(numberBracketsFullExpression > maxDeepBracket)
+                    {
+                        maxDeepBracket = numberBracketsFullExpression;
+                    }
+                }
+                else if (symbol== rightBracket)
+                {
+                    numberBracketsFullExpression += valueRightBracket;
+
+                    if(numberBracketsFullExpression < 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (numberBracketsFullExpression == 0)
+            {
+                Console.WriteLine($"Строка {userInput} корректная, максимальная глубина выражения {maxDeepBracket}");
+            }
+            else 
+            {
+                Console.WriteLine($"Строка {userInput} некорректна");
+            }
+        }
+    }
+}
