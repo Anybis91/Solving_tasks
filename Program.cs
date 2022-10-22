736d95d7f099de8cc760dc5ae6ce14301cd1550c
Harytamo <107985314+Anybis91@users.noreply.github.com>
2022-10-22 04:16:28 +0300

    Task_24.1

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNumber = new int[30] { 1, 1, 1, 1, 4, 3, 3, 3, 5, 6, 0, 8, 8, 8, 8, 8, 2, 4, 3, 2, 4, 2, 3, 4, 3, 4, 4, 5, 5, 5 };
+            int numberIteration = 0;
+            int numberInArray = 0;
+            int numberIterationTwo = 0;
+            int numberInArrayTwo = 0;
+
+            for (int i = 0; i < arrayNumber.Length; i++)
+            {
+                Console.Write(arrayNumber[i] + " ");
+             }
+
+            for (int i = 0; i < arrayNumber.Length; i++)
+            {
+                if (numberInArrayTwo == arrayNumber[i])
+                {
+                    numberIterationTwo++;
+                }
+                else
+                {
+                    if (numberIterationTwo > numberIteration)
+                    {
+                        numberIteration = numberIterationTwo;
+                        numberInArray = numberInArrayTwo;
+                    }
+                    else
+                    {
+                        numberInArrayTwo = arrayNumber[i];
+                        numberIterationTwo = 1;
+                    }
+                }
+            }
+
+            Console.WriteLine($"\nЧисло {numberInArray} повторяется {numberIteration} раз.");
+        }
+    }
+}
