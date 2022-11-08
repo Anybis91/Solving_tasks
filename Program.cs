795353fab28cae5af5edc253e79f83723162f0aa
Harytamo <107985314+Anybis91@users.noreply.github.com>
2022-11-08 17:03:57 +0300

    Task_26

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myText = "Съешь же ещё этих мягких французских булок, да выпей чаю!";
+            string[] subs = myText.Split(' ');
+
+            foreach (var sub in subs)
+            {
+                Console.WriteLine($"{sub}");
+            }
+        }
+    }
+}
