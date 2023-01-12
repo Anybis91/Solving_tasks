1f6f627bc584ac6cbb006518b172e6b9d7ea6986
Harytamo <107985314+Anybis91@users.noreply.github.com>
2023-01-12 13:12:05 +0300

    Task_29

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Task_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double healthRemaning;
+            double lengthBar = 100;
+            bool isWork = true;
+            Console.Write("Введите максимальное количество здоровья:");
+            double maxHealthUserInput = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите количество оставшегося здоровья:");
+            double healthUserInput = Convert.ToDouble(Console.ReadLine());
+            healthRemaning = (healthUserInput / maxHealthUserInput) * lengthBar;
+            Console.Write("Введите строку расположения бара:");
+            int position = Convert.ToInt32(Console.ReadLine());
+
+            while(isWork)
+            {
+                if (position < 2)
+                {
+                    Console.Write("Слишком близко к тексту!");
+                    isWork = false;
+                }
+                else
+                {
+                    DrawBar(healthRemaning, lengthBar, ConsoleColor.Red, position);
+                    Console.ReadKey();
+                    Console.Clear();
+                    isWork = false;
+                }
+            }
+        }
+
+        static void DrawBar(double value, double maxValue,ConsoleColor color,int position)
+        {
+            ConsoleColor defaultColor=Console.BackgroundColor;
+            string bar = "";
+
+            for(double i=0;i<value;i++)
+            {
+                bar += " "; 
+            }
+
+            Console.SetCursorPosition(0, position);
+            Console.Write('[');
+            Console.BackgroundColor=color;
+            Console.Write(bar);
+            Console.BackgroundColor = defaultColor;
+            bar = "";
+
+            for(double i=value;i<maxValue;i++)
+            {
+                bar += ' ';
+            }
+
+            Console.Write(bar + ']');
+        }
+    }
+}
