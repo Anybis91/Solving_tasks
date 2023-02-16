08655e475572cb00170c4f4fdde34723fd2bdc9b
Harytamo <107985314+Anybis91@users.noreply.github.com>
2023-02-16 22:14:50 +0300

    Kansas_city_shuffle

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task_Kansas_city_shuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[11] {1,2,3,4,5,6,8,9,3,5,7};
+            Console.WriteLine("Массив с данными:");
+            OutputArray(ref myArray);
+            Shuffle(ref myArray);
+            Console.WriteLine("\nПеремешанный массив:");
+            OutputArray(ref myArray);
+        }
+
+        static void Shuffle(ref int[] myArray)
+        {
+            Random randomNumberInArray = new Random();
+            int tempIndex;
+            int tempNumber;
+
+            for (int i = myArray.Length-1; i >=1; i--)
+            {
+                tempIndex = randomNumberInArray.Next(i+1);
+                tempNumber = myArray[tempIndex];
+                myArray[tempIndex] = myArray[i];
+                myArray[i]=tempNumber;
+            }
+        }
+
+        static void OutputArray(ref int[] myArray)
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write(myArray[i] + " ");
+            }
+        }
+    }
+}
